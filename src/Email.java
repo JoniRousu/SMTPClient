9dac9916b25d309c048a109b7efd092f9cd2b5ef
Joni Rousu <jonirousu@gmail.com>
2018-06-03 17:42:12 +0200

    MIME Support
    Added MIME support for messages.
    Messages are always encoded in Base64.

--- src/Email.java
+++ src/Email.java
@@ -1,27 +1,78 @@
+import java.util.Base64;
+
 
 public class Email {
 
 	public String SenderName;
-	public String SenderAddress;
+	private String SenderAddress;
 	public String ReceiverName;
 	public String ReceiverAddress;
-	public String message;
+	private String message;
 	public String Subject;
 	public String header;
+	private String attachment;
+	private String boundary;
 	
 	Email(){
-		SenderAddress = "";
+		setSenderAddress("");
 		SenderName = "";
 		ReceiverAddress = "";
 		ReceiverName = "";
 		Subject = "";
 		message = "";
+		boundary = "";
+		header = "";
+		attachment = "";
 	}
+	
+	
+	public String getSenderAddress() {
+		return SenderAddress;
+	}
+
+
+	public void setSenderAddress(String senderAddress) {
+		SenderAddress = senderAddress;
+		boundary = Base64.getMimeEncoder().encodeToString(getSenderAddress().getBytes());
+	}
+
+
+	public void setMessage(String Message){
+		message = Base64.getMimeEncoder().encodeToString(Message.getBytes());
+		if (attachment.isEmpty()){
+			message += "\n--" + boundary + "--";
+		}
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+	
+	
+	public String getAttachment() {
+		return attachment;
+	}
+
+
+	public void setAttachment(String attachment) {
+		this.attachment = attachment;
+	}
+
 	public String getHeader(){
 		String Header = "";
-		Header += "From: " + SenderName + " <" + SenderAddress + ">\n";
+		Header += "From: " + SenderName + " <" + getSenderAddress() + ">\n";
 		Header += "To: " + ReceiverName + " <" + ReceiverAddress + ">\n";
-		Header += "Subject: " + Subject + "\n";
+		if (!Subject.isEmpty()){
+			Header += "Subject: " + Subject + "\n";
+		}
+		//MIME
+		Header += "Mime-Version: 1.0\n";
+		Header += "Content-Type: multipart/mixed; boundary=\"" + boundary + "\"\n";
+		Header += "Content-Disposition: inline\n";
+		Header += "--" + boundary + "\n";
+		Header += "Content-Type: text/plain; charset=utf-8\n";
+		Header += "Content-Disposition: inline\n";
+		Header += "Content-Transfer-Encoding: base64\n";
 		
 		return Header;
 	}
