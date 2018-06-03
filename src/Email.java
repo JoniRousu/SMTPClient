e54520f477f27c23dc5401e6fcd93b0650f71678
Joni Rousu <jonirousu@gmail.com>
2018-06-03 16:16:00 +0200

    Email Class
    Removed MailHeader and added the header to the email itself
    TODO: MIME support

--- src/Email.java
+++ src/Email.java
@@ -6,12 +6,23 @@ public class Email {
 	public String ReceiverName;
 	public String ReceiverAddress;
 	public String message;
-	public MailHeader header = new MailHeader();
+	public String Subject;
+	public String header;
+	
 	Email(){
 		SenderAddress = "";
 		SenderName = "";
 		ReceiverAddress = "";
 		ReceiverName = "";
-		header.Subject = "";
+		Subject = "";
+		message = "";
+	}
+	public String getHeader(){
+		String Header = "";
+		Header += "From: " + SenderName + " <" + SenderAddress + ">\n";
+		Header += "To: " + ReceiverName + " <" + ReceiverAddress + ">\n";
+		Header += "Subject: " + Subject + "\n";
+		
+		return Header;
 	}
 }
--- src/MailHeader.java
+++
@@ -1,7 +0,0 @@
-public class MailHeader {
-	public String Subject;
-	//TODO: MIME
-	MailHeader(){
-		Subject = "";
-	}
-}
